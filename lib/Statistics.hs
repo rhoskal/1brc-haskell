d7073b676fb0d37afc822ff44580b8485325e2e4
Hans Hoffman <github.backrest158@passmail.net>
2024-07-23 00:02:48 +1200

    round when printing

--- lib/Statistics.hs
+++ lib/Statistics.hs
@@ -20,12 +20,12 @@ toString :: Statistics -> Text
 toString stat =
   (T.pack $ show $ sMin stat)
     <> T.singleton '/'
-    <> (T.pack $ show $ sMean stat)
+    <> (T.pack $ show $ roundTowardPositive $ sMean stat)
     <> T.singleton '/'
     <> (T.pack $ show $ sMax stat)
 
 roundTowardPositive :: (RealFrac a) => a -> Float
-roundTowardPositive n = fromIntegral ((ceiling $ n * 10) :: Integer) / 10.0 :: Float
+roundTowardPositive n = fromIntegral ((ceiling $ n * 10) :: Integer) / 10.0
 
 meanMaybe :: (Fractional a) => [a] -> Maybe a
 meanMaybe xs
@@ -34,15 +34,15 @@ meanMaybe xs
 
 min' :: [Celsius] -> Float
 min' [c] = unCelsius c
-min' cs = maybe (0.0 :: Float) unCelsius $ List.minimumMaybe cs
+min' cs = maybe 0.0 unCelsius $ List.minimumMaybe cs
 
 arithmeticMean :: [Celsius] -> Float
-arithmeticMean [c] = roundTowardPositive $ unCelsius c
-arithmeticMean cs = roundTowardPositive $ maybe (0.0 :: Float) unCelsius $ meanMaybe cs
+arithmeticMean [c] = unCelsius c
+arithmeticMean cs = maybe 0.0 unCelsius $ meanMaybe cs
 
 max' :: [Celsius] -> Float
 max' [c] = unCelsius c
-max' cs = maybe (0.0 :: Float) unCelsius $ List.maximumMaybe cs
+max' cs = maybe 0.0 unCelsius $ List.maximumMaybe cs
 
 mkStatistics :: [Celsius] -> Statistics
 mkStatistics cs = Statistics (min' cs) (arithmeticMean cs) (max' cs)
