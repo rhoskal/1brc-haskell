d7f45d99c2fe22e1a41e26a2e76f22b534e5a5c1
Hans Hoffman <github.backrest158@passmail.net>
2024-07-24 13:26:52 +1200

    fix tests

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -2,7 +2,7 @@ module ParserSpec (parserSpec) where
 
 import Parser
   ( Celsius (..),
-    Measurement (..),
+    Observation (..),
     Station (..),
     parser,
   )
@@ -11,12 +11,15 @@ import RIO.Text qualified as T
 import Test.Hspec
 import Test.QuickCheck
 
+genFracPart :: Gen Int
+genFracPart = choose (0, 9)
+
 parserSpec :: Spec
 parserSpec = do
   describe "parser" $ do
-    it "parses all possible Celsius values (neg & pos) correctly"
+    it "Should parse all possible Celsius values (neg & pos) correctly"
       $ property
-      $ \(intPart :: Int) (fracPart :: Int) ->
+      $ \(intPart :: Int) -> forAll genFracPart $ \fracPart ->
         let stationName :: Text
             stationName = "TestStation"
 
@@ -24,14 +27,15 @@ parserSpec = do
             celsiusText =
               (T.pack $ show intPart)
                 <> (T.singleton '.')
-                <> (T.pack $ show $ abs fracPart)
+                <> (T.pack $ show fracPart)
 
             input :: Text
             input = stationName <> T.singleton ';' <> celsiusText
          in case parser input of
-              Just (Measurement (Station s) (Celsius t)) ->
-                let maybeCelsius = readMaybe $ T.unpack celsiusText :: Maybe Float
-                 in s == stationName && (Just t) == maybeCelsius
+              Just (Observation (Station s) (Celsius c)) ->
+                let maybeCelsius :: Maybe Int16
+                    maybeCelsius = readMaybe $ T.unpack $ T.filter (/= '.') celsiusText
+                 in s == stationName && (Just c) == maybeCelsius
               _ -> False
     it "Should correctly handle an invalid row" $ do
       parser "Bogotá,12.0" `shouldBe` Nothing
